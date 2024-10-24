5bb0824fc1adaea276443f8c03e2cea455a090a5
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-10-24 12:08:52 +0000

    fix all errors?

--- integration_tests/commands/websocket/exists_test.go
+++ integration_tests/commands/websocket/exists_test.go
@@ -3,7 +3,7 @@ package websocket
 import (
 	"testing"
 
-	"gotest.tools/v3/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestExists(t *testing.T) {
@@ -13,30 +13,31 @@ func TestExists(t *testing.T) {
 		{
 			name:     "Test EXISTS command",
 			commands: []string{"SET key value", "EXISTS key", "EXISTS key2"},
-			expected: []interface{}{"OK", int64(1), int64(0)},
+			expected: []interface{}{"OK", float64(1), float64(0)},
 		},
 		{
 			name:     "Test EXISTS command with multiple keys",
 			commands: []string{"SET key value", "SET key2 value2", "EXISTS key key2 key3", "EXISTS key key2 key3 key4", "DEL key", "EXISTS key key2 key3 key4"},
-			expected: []interface{}{"OK", "OK", int64(2), int64(2), int64(1), int64(1)},
+			expected: []interface{}{"OK", "OK", float64(2), float64(2), float64(1), float64(1)},
 		},
 		{
 			name:     "Test EXISTS an expired key",
 			commands: []string{"SET key value ex 1", "EXISTS key", "EXISTS key"},
-			expected: []interface{}{"OK", int64(1), int64(0)},
+			expected: []interface{}{"OK", float64(1), float64(0)},
 		},
 		{
 			name:     "Test EXISTS with multiple keys and expired key",
 			commands: []string{"SET key value ex 2", "SET key2 value2", "SET key3 value3", "EXISTS key key2 key3", "EXISTS key key2 key3"},
-			expected: []interface{}{"OK", "OK", "OK", int64(3), int64(2)},
+			expected: []interface{}{"OK", "OK", "OK", float64(3), float64(2)},
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			conn := exec.ConnectToServer()
 			for i, cmd := range tc.commands {
-				result := exec.FireCommand(conn, cmd)
-				assert.Equal(t, tc.expected[i], result, "Value mismatch for cmd %s", cmd)
+				result, err := exec.FireCommandAndReadResponse(conn, cmd)
+				assert.Nil(t, err)
+				assert.Equal(t, tc.expected[i], result)
 			}
 		})
 	}
